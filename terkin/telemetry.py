9c60507553f667b53947bcd7e8981ec7daffb2da
Andreas Motl <andreas@hiveeyes.org>
2019-03-07 04:25:48 +0100

    Improve robustness of TelemetryClient

--- terkin/telemetry.py
+++ terkin/telemetry.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 # (c) 2017-2019 Andreas Motl <andreas@terkin.org>
 # License: GNU General Public License, Version 3
+# https://github.com/daq-tools/terkin/blob/0.0.2/src/micropython/uterkin/telemetry.py
 
-# micropython -m upip install micropython-json micropython-copy
+# Pycom builtin.
+# Otherwise: micropython -m upip install micropython-json micropython-copy
 import json
 
 # Problem: "urequests" does not work with SSL, e.g. https://httpbin.org/ip
 # micropython -m upip install micropython-urequests
 #import urequests
 
+# micropython -m upip install micropython-copy
+from copy import copy
 
+# FIXME: Touch me after using an appropriate "urllib" library.
+# See also: https://forum.pycom.io/topic/4494/libpcre-missing
+# micropython -m upip install micropython-urllib.parse
 try:
-    from urllib import urlsplit, urlencode
-    from copy import copy
+    from urllib.parse import urlsplit, urlencode
 except:
-    pass
+    print('WARNING: libpcre is not available on this platform. TelemetryClient will talk MQTT only.')
 
 
 class TelemetryClient:
@@ -41,6 +47,8 @@ class TelemetryClient:
         self.suffixes = suffixes or {}
 
         """
+        # FIXME: Touch me after using an appropriate "urllib" library.
+        # See also: https://forum.pycom.io/topic/4494/libpcre-missing
         self.scheme, self.netloc, self.path, self.query, self.fragment = urlsplit(self.uri)
 
         if self.scheme in ['http', 'https']:
@@ -120,6 +128,9 @@ class TelemetryTransportHTTP:
 
         self.uri = uri
         self.format = format
+
+        # FIXME: Touch me after using an appropriate "urllib" library.
+        # See also: https://forum.pycom.io/topic/4494/libpcre-missing
         self.scheme, self.netloc, self.path, self.query, self.fragment = urlsplit(self.uri)
 
         # Does not work
@@ -158,41 +169,96 @@ class TelemetryTransportHTTP:
 
 
 class TelemetryTransportMQTT:
+    """
+    MQTT transport for Terkin Telemetry.
+
+    This is currently based on the "Pycom MicroPython MQTT module" just called ``mqtt.py``.
+    https://github.com/pycom/pycom-libraries/blob/master/lib/mqtt/mqtt.py
+
+    Originally, this was based on the "umqtt.robust" library::
+
+        micropython -m upip install micropython-umqtt.robust micropython-umqtt.simple
+
+    TODO: Try to make this module reasonably compatible again
+          by becoming an adapter for different implementations.
+    """
 
     def __init__(self, uri, format):
 
-        print('TelemetryTransportMQTT')
+        print('Telemetry transport: MQTT over TCP over WiFi')
         self.uri = uri
         self.format = format
+        self.defunct = False
+        self.defunctness_reported = False
 
-        try:
-            self.start()
-        except Exception as ex:
-            print('ERROR: Failed to connect to MQTT broker. {}'.format(ex))
+        # TODO: Start connecting to MQTT broker here already?
+        #       Maybe we should defer this to the point where the
+        #       first transmission will require it.
+        self.start()
 
     def start(self):
 
-        # TODO: Add multiadapter thing, currently based on mqtt.py by Pycom.
-        # INACTIVE: micropython -m upip install micropython-umqtt.robust micropython-umqtt.simple
-        from mqtt import MQTTClient
-
+        # FIXME: Touch me after using an appropriate "urllib" library.
+        # See also: https://forum.pycom.io/topic/4494/libpcre-missing
         # self.scheme, self.netloc, self.path, self.query, self.fragment = urlsplit(self.uri)
-        self.connection = MQTTClient("umqtt_client", "swarm.hiveeyes.org")
-        self.connection.DEBUG = True
-        self.connection.connect()
+
+        # Load MQTT module.
+        # TODO: Create abstract MQTT client factory to account for different implementations.
+        try:
+            from mqtt import MQTTClient
+
+        except Exception as ex:
+            print('ERROR: Loading MQTT module failed: {}'.format(ex))
+            self.defunct = True
+            return False
+
+        # Connect to MQTT broker.
+        # TODO: Try continuous reconnection to MQTT broker.
+        try:
+            # TODO: Use MQTT broker value from configuration settings.
+            # TODO: Use device identifier here to make the MQTT client id more unique.
+            self.connection = MQTTClient("umqtt_client", "swarm.hiveeyes.org")
+            self.connection.DEBUG = True
+            self.connection.connect()
+
+        except Exception as ex:
+            print('ERROR: Connecting to MQTT broker failed: {}'.format(ex))
+            self.defunct = True
+            return False
+
+        return True
 
     def send(self, request_data):
+
+        if self.defunct:
+            if not self.defunctness_reported:
+                print('ERROR: MQTT transport is defunct, please scan log '
+                      'output for previous error messages.')
+                self.defunctness_reported = True
+            return False
+
+        # TODO: Use MQTT topic value from configuration settings.
         topic = "hiveeyes/testdrive/irgendwas/baz/data.json"
-        print('MQTT Topic:  ', topic)
-        print('Payload:     ', request_data['payload'])
+        print('MQTT topic:  ', topic)
+        print('MQTT payload:', request_data['payload'])
         self.connection.publish(topic, request_data['payload'])
         return True
 
 
 class TelemetryTopologies:
+    """
+    Define **how** to communicate using Telemetry.
+    """
 
     class KotoriWanTopology:
+        """
+        This defines how to communicate in WAN scenarios having a decent
+        number of devices rolled out. While this would cover even earth-scale
+        addressing scenarios, it will also give you peace of mind in smaller
+        setups, even in multi-project or multi-tenant environments.
 
+        Just trust us and keep it as a default setting for your journey ;].
+        """
         uri_template = u'{base_uri}/{realm}/{network}/{gateway}/{node}'
         suffixes = {
             TelemetryClient.TRANSPORT_HTTP: '/data',
@@ -220,14 +286,14 @@ class TelemetryNode:
             self.suffixes = topology.suffixes
 
     def format_uri(self, **kwargs):
-        data = self.address
+        data = copy(self.address)
         data.update(kwargs)
         return self.uri_template.format(**data)
 
     def setup(self):
         self.channel_uri = self.format_uri()
 
-        print('Channel URI: ', self.channel_uri)
+        print('Telemetry channel URI: ', self.channel_uri)
         self.client = self.client_factory()
 
     def client_factory(self):
@@ -240,7 +306,9 @@ class TelemetryNode:
 
 class CSVTelemetryNode(TelemetryNode):
     """
-    Telemetry node client: Network participant API
+    Telemetry node client: Network participant API.
+
+    This will make your node talk CSV.
     """
 
     def __init__(self, *args, **kwargs):
@@ -249,5 +317,5 @@ class CSVTelemetryNode(TelemetryNode):
 
     def transmit(self, data, **kwargs):
         uri = self.format_uri(**kwargs)
-        print('Channel URI: ', uri)
-	return self.client.transmit(data, uri=uri, serialize=False)
+        print('Telemetry channel URI for CSV: ', uri)
+        return self.client.transmit(data, uri=uri, serialize=False)
