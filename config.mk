80bd37e3cc763068e7b833e55b8c9b61b2aa7c44
Andreas Motl <andreas@hiveeyes.org>
2019-03-16 01:49:43 +0100

    Reduce rshell buffer size to "30"

--- config.mk
+++ config.mk
@@ -5,5 +5,5 @@
 # Serial interface settings
 # =========================
 $(eval serial_port     := /dev/cu.usbmodemPye090a1)
-$(eval serial_bufsize  := 2048)
+$(eval serial_bufsize  := 30)
 $(eval rshell_options  := --port $(serial_port) --buffer-size $(serial_bufsize) --timing)
