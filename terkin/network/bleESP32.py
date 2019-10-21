533b226d0e27df2f2d3067496d12aedf13f454fe
Markus <markus@johalla.de>
2019-10-21 12:18:48 +0200

    fixed advertising

--- terkin/network/bleESP32.py
+++ terkin/network/bleESP32.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # License: GNU General Public License, Version 3
 
+import struct, time, random
 from bluetooth import BLE, UUID, FLAG_NOTIFY, FLAG_READ, FLAG_WRITE
 from micropython import const
 from ble_advertising import advertising_payload
-from terkin import logging
+#from terkin import logging
 
 _IRQ_CENTRAL_CONNECT                 = const(1 << 0)
 _IRQ_CENTRAL_DISCONNECT              = const(1 << 1)
@@ -45,71 +46,159 @@ _GA_SERVICE = (_GA_UUID, (_GA_CHAR_NAME, _GA_CHAR_APP,), )
 #_CT_SERVICE = (_CT_UUID, (_CT_CHAR,), )
 
 SERVICES = (_BS_SERVICE, _WS_SERVICE, _ES_SERVICE, _GA_SERVICE,)
+#SERVICES = (_BS_SERVICE, _WS_SERVICE, _ES_SERVICE, )
+#SERVICES = (_WS_SERVICE, )
 
 # set appearance - sets the icon
 # org.bluetooth.characteristic.gap.appearance.xml
 _ADV_APPEARANCE_GENERIC_WEIGHT_SCALE = const(3200)
 
-log = logging.getLogger(__name__)
+#log = logging.getLogger(__name__)
 
 class BluetoothApiESP32:
     """
     
     """
 
-    def __init__(self, ble, name='hiveeyes'):
+    def __init__(self, ble, name='h'):
         """
         """
-        self._reading = reading  # last_reading from datalogger.storage
+        #self._reading = reading  # last_reading from datalogger.storage
         self._ble = ble
+        print('Starting BLE')
         self.start()    
         self._ble.irq(handler=self._irq)
-        ((self._handle,),) = self._ble.gatts_register_services(SERVICES)
+        #((self._WS_handle,),(self._BS_handle,),(self._ES_HUM_handle, self._ES_TEMP_handle,),) = self._ble.gatts_register_services(SERVICES)
+        ((self._WS_handle,),(self._BS_handle,),(self._ES_HUM_handle, self._ES_TEMP_handle,),(self._GA_NAME_handle,self._GA_APP_handle,),) = self._ble.gatts_register_services(SERVICES)
         self._connections = set()
         # advertise that we are here and what services we provide
-        self._payload = advertising_payload(name=name, services=([0x180F],[0x181A],[0x181D],[0x1800]), appearance=_ADV_APPEARANCE_GENERIC_THERMOMETER)
+        self._payload = advertising_payload(name=name, services=([0x180F,0x181A,0x181D,0x1800]), appearance=_ADV_APPEARANCE_GENERIC_WEIGHT_SCALE)
+        print('Start advertising')
         self._advertise()
 
+    def _irq(self, event, data):
+        # Track connections so we can send notifications.
+        if event == _IRQ_CENTRAL_CONNECT:
+            conn_handle, _, _, = data
+            self._connections.add(conn_handle)
+        elif event == _IRQ_CENTRAL_DISCONNECT:
+            conn_handle, _, _, = data
+            self._connections.remove(conn_handle)
+            # Start advertising again to allow a new connection.
+            self._advertise()
 
     def start(self):
-        log.info('Starting Bluetooth')
+        #log.info('Starting Bluetooth')
+        print('Starting Bluetooth')
         self._ble.active(True)
 
     def stop(self):
-        log.info('Stopping Bluetooth')
+        #log.info('Stopping Bluetooth')
+        print('Stopping Bluetooth')
         self._ble.active(False)
 
-    def _advertise(self, interval_us=1000000):
+    def _advertise(self, interval_us=500000):
         self._ble.gap_advertise(interval_us, adv_data=self._payload)
 
     def set_battery_level(self, level=0, notify=False): 
         """
-        level in [%]
-        Data is uint8.
+        Level in [%]
+        Data is uint8
+        Resolution: 1
         """
-        self._ble.gatts_write(self._handle, struct.pack('<B', int(level)))
+        self._ble.gatts_write(self._BS_handle, struct.pack('<B', int(level)))
+        print('Write BS: ', struct.pack('<B', int(level)))
         if notify:
             for conn_handle in self._connections:   # Notify connected centrals to issue a read.
-                self._ble.gatts_notify(conn_handle, self._handle)        
+                self._ble.gatts_notify(conn_handle, self._BS_handle)     
+                print('Notify BS handle: ',self._BS_handle)   
 
     def set_weight(self, weight=0.0, notify=False): 
         """
         weight in [kg]
-        Data is uint16. Unit is in kilograms with a resolution of 0.005.
+        Data is uint16
+        Resolution: 0.005
         """
-        self._ble.gatts_write(self._handle, struct.pack('<H', int(weight/0.005)))
+        self._ble.gatts_write(self._WS_handle, struct.pack('<H', int(weight/0.005)))
         if notify:
             for conn_handle in self._connections:   # Notify connected centrals to issue a read.
-                self._ble.gatts_notify(conn_handle, self._handle)
+                self._ble.gatts_notify(conn_handle, self._WS_handle)
+
+    def set_humidity(self, humidity=0.0, notify=False): 
+        """
+        humidity in [%]
+        Data is uint16. 
+        Resolution: 0.01.
+        """
+        self._ble.gatts_write(self._ES_HUM_handle, struct.pack('<H', int(humidity/0.01)))
+        if notify:
+            for conn_handle in self._connections:   # Notify connected centrals to issue a read.
+                self._ble.gatts_notify(conn_handle, self._ES_HUM_handle)
+
+    def set_temperature(self, temperature=0.0, notify=False): 
+        """
+        temperature in [°C]
+        Data is sint16. 
+        Resolution of 0.01.
+        """
+        self._ble.gatts_write(self._ES_TEMP_handle, struct.pack('<h', int(temperature/0.01)))
+        if notify:
+            for conn_handle in self._connections:   # Notify connected centrals to issue a read.
+                self._ble.gatts_notify(conn_handle, self._ES_TEMP_handle)
+
+    def set_name(self, name='Hiveeyes', notify=False): 
+        """
+        Data is utf8s. 
+        """
+        pass
+        #self._ble.gatts_write(self._ES_TEMP_handle, struct.pack('<h', int(temperature/0.01)))
+        if notify:
+            for conn_handle in self._connections:   # Notify connected centrals to issue a read.
+                self._ble.gatts_notify(conn_handle, self._ES_TEMP_handle)
+
+    def set_appearance(self, appearance=3200, notify=False): 
+        """
+        Data is ? 
+        """
+        pass
+        #self._ble.gatts_write(self._ES_TEMP_handle, struct.pack('<h', int(temperature/0.01)))
+        if notify:
+            for conn_handle in self._connections:   # Notify connected centrals to issue a read.
+                self._ble.gatts_notify(conn_handle, self._ES_TEMP_handle)
 
 def demo():
-    ble = bluetooth.BLE()
+    ble = BLE()
     ble32 = BluetoothApiESP32(ble)
 
-    while True:
-        print('loop')
-        time.sleep_ms(1000)
+    bat = 50
+    kg = 30.0
+    hum = 60.0
+    temp = 25.0
 
+    i = 0
+
+    while True:
+        # Write every second, notify every 10 seconds.
+        i += 1
+        if i % 3 == 0:
+            ble32.set_battery_level(int(bat), notify=True)
+            print('bat: ',bat)
+        if i % 5 == 0:    
+            ble32.set_weight(kg, notify=True)
+            print(' kg: ',kg)
+        if i % 7 == 0:
+            ble32.set_humidity(hum, notify=True)
+            print('  hum: ',hum)
+        if i % 11 == 0:
+            ble32.set_temperature(temp, notify=True)
+            print('   temp: ',temp)
+
+        # Random walk 
+        bat += int(random.uniform(-5, 5))
+        kg += random.uniform(-0.5, 0.5)
+        hum += random.uniform(-0.5, 0.5)
+        temp += random.uniform(-0.5, 0.5)
+        time.sleep_ms(1000)
 
 if __name__ == '__main__':
     demo()
