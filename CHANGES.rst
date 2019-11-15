65638575dfe14e2d5da768af6e1cd8a6b7b06285
Andreas Motl <andreas@hiveeyes.org>
2019-11-15 19:43:30 +0100

    Upgrade to MicroWebSrv2 2.0.3

--- CHANGES.rst
+++ CHANGES.rst
@@ -87,6 +87,7 @@ Development
 - Upgrade to MicroWebSrv2 2.0.2
 - Catch ``KeyboardInterrupt`` exceptions in WiFi keepalive thread and
   ``UdpServer`` in order to shut down using a single ``CTRL-C``
+- Upgrade to MicroWebSrv2 2.0.3. Thanks, @jczic!
 
 
 2019-08-19 0.6.0
--- tools/setup.mk
+++ tools/setup.mk
@@ -43,7 +43,7 @@ download-requirements:
 	#$(fetch) $(target_dir) https://raw.githubusercontent.com/jczic/MicroDNSSrv/4cd90f6/microDNSSrv.py
 
 	# Install MicroWebSrv2
-	curl --location https://github.com/jczic/MicroWebSrv2/archive/v2.0.2.zip | tar -C $(target_dir) --strip-components=1 -xvf - MicroWebSrv2-2.0.2/MicroWebSrv2
+	curl --location https://github.com/jczic/MicroWebSrv2/archive/v2.0.3.zip | tar -C $(target_dir) --strip-components=1 -xvf - MicroWebSrv2-2.0.3/MicroWebSrv2
 
 	# Install BLE GATTS Wrapper for Pycom devices
 	# https://github.com/cmisztur/pycom-ble-gatt-wrapper
