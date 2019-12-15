48bb7d0997aa2a9b3422bf0c9151a112dfa8ffa1
Matthias Büchner <m.buechner@gmail.com>
2019-12-16 00:18:29 +0100

    skip LoRa initialization for non-LoPy devices

--- terkin/device.py
+++ terkin/device.py
@@ -87,19 +87,22 @@ class TerkinDevice:
             log.info("[WiFi] interface disabled in settings.")
 
         # Initialize LoRa device.
-        if self.settings.get('networking.lora.enabled'):
-            if self.settings.get('networking.lora.antenna_attached'):
-                try:
-                    self.networking.start_lora()
-                    self.status.networking = True
-                except Exception as ex:
-                    log.exc(ex, 'Unable to start LoRa subsystem')
-                    self.status.networking = False
+        if "LoPy" in self.application_info.platform_info.device_name:
+            if self.settings.get('networking.lora.enabled'):
+                if self.settings.get('networking.lora.antenna_attached'):
+                    try:
+                        self.networking.start_lora()
+                        self.status.networking = True
+                    except Exception as ex:
+                        log.exc(ex, 'Unable to start LoRa subsystem')
+                        self.status.networking = False
+                else:
+                    log.info("[LoRa] Disabling LoRa interface as no antenna has been attached. "
+                                 "ATTENTION: Running LoRa without antenna will wreck your device.")
             else:
-                log.info("[LoRa] Disabling LoRa interface as no antenna has been attached. "
-                         "ATTENTION: Running LoRa without antenna will wreck your device.")
+                log.info("[LoRa] interface disabled in settings. Skip LoRa initialisation.")
         else:
-            log.info("[LoRa] interface disabled in settings.")
+            log.info("[LoRa] This is not a LoRa capable device.")
 
         # Inform about networking status.
         #self.networking.print_status()
