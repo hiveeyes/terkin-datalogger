793f1485a412ddb2729d459c19a11d0fb24a3468
Andreas Motl <andreas@hiveeyes.org>
2020-04-29 03:01:54 +0200

    Improve CPython requirements

--- tools/cpython.mk
+++ tools/cpython.mk
@@ -15,8 +15,5 @@ install-cpython:
 	# Install updated pySX127x driver.
 	curl --location https://github.com/daq-tools/pySX127x/archive/dragino.tar.gz | tar -C $(target_dir)/dragino --strip-components=1 -xzvf - pySX127x-dragino/SX127x
 
-	# Install Victron Energy VE.Direct text protocol driver.
-	$(pip) install git+https://github.com/karioja/vedirect@f74c0f2
-
 run-cpython:
 	.venv3/bin/python src/main_cpython.py
