0f7fccb911ae37efe74ccc4c3c3cf313bd66a7ec
Matthias Büchner <m.buechner@gmail.com>
2020-04-30 15:13:28 +0200

    import scale parameters as floating point number

--- src/lib/terkin/datalogger.py
+++ src/lib/terkin/datalogger.py
@@ -457,8 +457,8 @@ class TerkinDatalogger:
             sensor_object.set_address(sensor_info.get('number', sensor_info.get('address', 0)))
             sensor_object.register_pin('dout', sensor_info['pin_dout'])
             sensor_object.register_pin('pdsck', sensor_info['pin_pdsck'])
-            sensor_object.register_parameter('scale', sensor_info['scale'])
-            sensor_object.register_parameter('offset', sensor_info['offset'])
+            sensor_object.register_parameter('scale', float(sensor_info['scale']))
+            sensor_object.register_parameter('offset', float(sensor_info['offset']))
             sensor_object.register_parameter('gain', sensor_info.get('gain', 128))
 
             # Select driver module. Use "gerber" (vanilla) or "heisenberg" (extended).
