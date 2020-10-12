843fa513da9d48c43253087582acb5d1af045140
Werner Sturm <wernersturm89@gmail.com>
2020-10-12 15:35:35 +0200

    - moved docker related files into folder "docker" - added putsreq with reverse-proxy to docker-compose

--- docker/init-volumes.sh
+++ docker/init-volumes.sh
@@ -8,8 +8,18 @@ mkdir "./${VOLUMES_DIR}/mosquitto/log"
 
 mkdir -p "./${VOLUMES_DIR}/influxdb/data"
 mkdir "./${VOLUMES_DIR}/influxdb/conf"
+mkdir "./${VOLUMES_DIR}/influxdb/log"
 
 mkdir -p "./${VOLUMES_DIR}/grafana/data"
 mkdir "./${VOLUMES_DIR}/grafana/log"
 
 mkdir -p "./${VOLUMES_DIR}/kotori/conf/apps-enabled"
+mkdir "./${VOLUMES_DIR}/kotori/log"
+
+mkdir -p "./${VOLUMES_DIR}/mongodb/data"
+mkdir "./${VOLUMES_DIR}/mongodb/log"
+
+mkdir -p "./${VOLUMES_DIR}/redis/data"
+
+mkdir -p "./${VOLUMES_DIR}/nginx/cert"
+mkdir "./${VOLUMES_DIR}/nginx/conf.d"
