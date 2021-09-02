6c8fa389b0ebd27eef4af7f234b8b32df8cd102d
Manu Lange <Manu.Lange@plantandfood.co.nz>
2021-09-02 04:19:33 +0000

    VEDirect driver: Add support for MicroPython

--- src/lib/terkin/driver/vedirect_sensor.py
+++ src/lib/terkin/driver/vedirect_sensor.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # (c) 2020 Jan Hoffmann <jan.hoffmann@bergamsee.de>
 # (c) 2020 Andreas Motl <andreas.motl@terkin.org>
+# (c) 2021 Manu Lange <Manu.Lange@plantandfood.co.nz>
 # License: GNU General Public License, Version 3
 from terkin import logging
 from terkin.sensor import SensorManager, AbstractSensor
@@ -39,6 +40,7 @@ class VEDirectSensor(AbstractSensor):
     Resources
     =========
     - https://github.com/karioja/vedirect
+    - https://github.com/nznobody/vedirect
     - https://www.victronenergy.com/solar-charge-controllers/smartsolar-mppt-75-10-75-15-100-15-100-20
     - https://www.victronenergy.com/solar-charge-controllers/bluesolar-mppt-150-35
     - https://www.victronenergy.com/battery-monitors/bmv-700
@@ -51,7 +53,7 @@ class VEDirectSensor(AbstractSensor):
         super().__init__(settings=settings)
 
         # Can be overwritten by ``.set_address()``.
-        self.device = settings['device']
+        self.device = settings["device"]
         self.timeout = 5
         self.driver = None
 
@@ -62,36 +64,51 @@ class VEDirectSensor(AbstractSensor):
         try:
 
             # MicroPython
-            if platform_info.vendor in [platform_info.MICROPYTHON.Vanilla, platform_info.MICROPYTHON.Pycom]:
-                raise NotImplementedError('VEDirect driver not implemented on MicroPython')
+            if platform_info.vendor in [
+                platform_info.MICROPYTHON.Vanilla,
+                platform_info.MICROPYTHON.Pycom,
+            ]:
+                try:
+                    from vedirect import VEDirect
+
+                    uart = int(self.device)
+                    self.driver = VEDirect(serialport=uart, timeout=self.timeout)
+                except Exception as e:
+                    log.exc(
+                        e,
+                        "Could not start VEDirect interface on device: {}".format(
+                            self.device
+                        ),
+                    )
+                    raise e
 
             # CPython
             elif platform_info.vendor == platform_info.MICROPYTHON.RaspberryPi:
-                from vedirect import Vedirect
-                self.driver = Vedirect(serialport=self.device, timeout=self.timeout)
+                from vedirect import VEDirect
+
+                self.driver = VEDirect(serialport=self.device, timeout=self.timeout)
 
             else:
-                raise NotImplementedError('VEDirect driver not implemented on this platform')
+                raise NotImplementedError(
+                    "VEDirect driver not implemented on this platform"
+                )
 
             return True
 
         except Exception as ex:
-            log.exc(ex, 'VEDirect hardware driver failed')
+            log.exc(ex, "VEDirect hardware driver failed")
 
     def read(self):
+        if not self.driver:
+            log.error("VEDirect interface not initialised")
+            return
         log.info('Reading sensor "Victron Energy VE.Direct"')
 
-        # MicroPython
-        if platform_info.vendor in [platform_info.MICROPYTHON.Vanilla, platform_info.MICROPYTHON.Pycom]:
-            raise NotImplementedError('VEDirect driver not implemented on MicroPython')
-
-        # CPython
-        elif platform_info.vendor == platform_info.MICROPYTHON.RaspberryPi:
-            data_raw = self.driver.read_data_single()
+        data_raw = self.driver.read_data_single()
 
         data = {}
         for key, value in data_raw.items():
-            key = 'vedirect:{}'.format(key)
+            key = "vedirect:{}".format(key)
             data[key] = value
 
         return data
