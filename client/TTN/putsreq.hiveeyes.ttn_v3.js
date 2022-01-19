30350b9f926dbfc1f8d3852f24490da2bc59e3d1
Matthias Büchner <m.buechner@gmail.com>
2022-01-19 12:23:09 +0100

    Putsreq payload extractor: use gateway ID from payload

--- client/TTN/putsreq.hiveeyes.ttn_v3.js
+++ client/TTN/putsreq.hiveeyes.ttn_v3.js
@@ -14,11 +14,10 @@ output.freq = input.uplink_message.settings.frequency / 1000000.0;
 output.counter = input.uplink_message.f_cnt;
 output.gtw_count = input.uplink_message.rx_metadata.length;
 
-/* ToDo: use gateway ID instead of array index once it is included in the TTN payload.
-   Perhaps when uplinks route directly via V3 Gateways */
 for (i = 0; i < output.gtw_count; i++) {
-  output["gw_" + i + "_rssi"] = input.uplink_message.rx_metadata[i].rssi;
-  output["gw_" + i + "_snr"]  = input.uplink_message.rx_metadata[i].snr;
+  gwid = input.uplink_message.rx_metadata[i].gateway_ids.gateway_id
+  output["gw_" + gwid + "_rssi"] = input.uplink_message.rx_metadata[i].rssi;
+  output["gw_" + gwid + "_snr"]  = input.uplink_message.rx_metadata[i].snr;
 }
 
 output.dev_id = input.end_device_ids.device_id;
